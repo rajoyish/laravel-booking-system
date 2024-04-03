60bcea24b93ac21386f64fb7dc53eb9e18d2f556
Rajesh Budhathoki <rajoyish@gmail.com>
2024-04-03 08:51:27 +0545

    Subtracting employee schedule exclusions

--- app/Bookings/ScheduleAvailability.php
+++ app/Bookings/ScheduleAvailability.php
@@ -3,9 +3,11 @@
 namespace App\Bookings;
 
 use App\Models\Employee;
+use App\Models\ScheduleExclusion;
 use App\Models\Service;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
+use Spatie\Period\Boundaries;
 use Spatie\Period\Period;
 use Spatie\Period\PeriodCollection;
 use Spatie\Period\Precision;
@@ -24,18 +26,36 @@ class ScheduleAvailability
         collect(CarbonPeriod::create($startsAt, $endsAt)->days())
             ->each(function ($date) {
                 $this->addAvailabilityFromSchedule($date);
+
+                $this->employee->scheduleExclusions->each(function (ScheduleExclusion $exclusion) {
+                    $this->subtractScheduleExclusion($exclusion);
+                });
             });
 
-        dd($this->periods);
+        foreach ($this->periods as $period) {
+            dump($period->asString());
+        }
+    }
+
+    protected function subtractScheduleExclusion(ScheduleExclusion $exclusion)
+    {
+        $this->periods = $this->periods->subtract(
+            Period::make(
+                $exclusion->starts_at,
+                $exclusion->ends_at,
+                Precision::MINUTE(),
+                Boundaries::EXCLUDE_END(),
+            )
+        );
     }
 
     public function addAvailabilityFromSchedule(Carbon $date)
     {
-        if (! $schedule = $this->employee->schedules->where('starts_at', '<=', $date)->where('ends_at', '>=', $date)->first()) {
+        if (!$schedule = $this->employee->schedules->where('starts_at', '<=', $date)->where('ends_at', '>=', $date)->first()) {
             return;
         }
 
-        if (! [$startsAt, $endsAt] = $schedule->getWorkingHoursFromDate($date)) {
+        if (![$startsAt, $endsAt] = $schedule->getWorkingHoursFromDate($date)) {
             return;
         }
 
