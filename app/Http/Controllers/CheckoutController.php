3f3096a8ea6b34de15447c54ab17e5839fd33731
Rajesh Budhathoki <rajoyish@gmail.com>
2024-04-28 09:19:55 +0545

    Scaffolding the checkout

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Employee $employee, Service $service)
+    {
+        abort_unless($employee->services->contains($service), 404);
+
+        // availability checks
+
+        return view('bookings.checkout', [
+            'employee' => $employee,
+            'service' => $service,
+        ]);
+    }
+}
